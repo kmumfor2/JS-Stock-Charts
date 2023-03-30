bdcae4255151a0067da37033e63aa09c25546d6a
kmumfor2 <120679405+kmumfor2@users.noreply.github.com>
2023-03-30 17:41:11 -0400

    Added my project

--- public/index.js
+++ public/index.js
@@ -6,4 +6,38 @@ async function main() {
 
 }
 
-main()
+Authorization="apikey_4d6de3f6900d449d85380b06a0a5d514"
+
+main(/time_series?symbol=GME,MSFT,DIS,BNTX)
+
+let GME = result.GME
+let MSFT = result.MSFT
+let DIS = result.DIS
+let BTNX = result.BTNX
+
+
+const { GME, MSFT, DIS, BNTX } = mockData;
+
+const stocks = [GME, MSFT, DIS, BNTX];
+
+//Chart
+stocks.forEach( stock => stock.values.reverse())
+
+// Time Chart
+new Chart(timeChartCanvas.getContext('2d'), {
+    type: 'line',
+    data: {
+        labels: stocks[0].values.reverse().map(value => value.datetime),
+        datasets: stocks.map(stock => ({
+            label: stock.meta.symbol,
+            data: stock.values.reverse().map(value => parseFloat(value.high))
+            backgroundColor: getColor(stock.meta.symbol),
+            borderColor: getColor(stock.meta.symbol),
+        }))
+    }
+});
+
+
+
+stocks[0].values.map( value => value. _)
+
